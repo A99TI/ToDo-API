8870ed851c0dc4e6bee77a08c1cffc017425d2c8
Attiq <attiqbashir0@gmail.com>
2025-12-31 13:19:10 +0000

    added tag & response for userController

--- src/main/java/com/a99ti/todo/controller/UserController.java
+++ src/main/java/com/a99ti/todo/controller/UserController.java
@@ -4,8 +4,10 @@ import com.a99ti.todo.entity.User;
 import com.a99ti.todo.request.PasswordUpdateRequest;
 import com.a99ti.todo.response.UserResponse;
 import com.a99ti.todo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,16 +21,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "User Information", description = "Get current user info")
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/info")
     public UserResponse getUserInfo(){
         return userService.getUserInfo();
     }
 
+    @Operation(summary = "Delete user", description = "Delete User Account")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping
     public void deleteUser(){
         userService.deleteUser();
     }
 
+    @Operation(summary = "Password update", description = "Change user password after verification")
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/password")
     public void passwordUpdate(@Valid @RequestBody PasswordUpdateRequest passwordUpdateRequest)
             throws Exception {
