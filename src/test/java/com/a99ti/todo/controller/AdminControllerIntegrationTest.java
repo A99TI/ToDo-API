e854bc1f5f17923f9c22b6eeded20892fe7cfc45
Attiq <attiqbashir0@gmail.com>
2026-01-06 09:53:06 +0000

    Update admin user integration test assertions

--- src/test/java/com/a99ti/todo/controller/AdminControllerIntegrationTest.java
+++ src/test/java/com/a99ti/todo/controller/AdminControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.a99ti.todo.controller;
 import com.a99ti.todo.entity.Authority;
 import com.a99ti.todo.entity.User;
 import com.a99ti.todo.repository.UserRepository;
+import com.a99ti.todo.response.UserResponse;
 import com.a99ti.todo.util.UserTestUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +14,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
@@ -20,7 +22,11 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -48,22 +54,20 @@ public class AdminControllerIntegrationTest {
 
     @Test
     void getAllUsers_ShouldReturnAllUsers() throws Exception{
-
-        User user = userTestUtil.createAndSaveDefaultAdminUser();
-
-        UsernamePasswordAuthenticationToken auth = userTestUtil.createAuthenticationToken(user);
+        User adminUser = userTestUtil.createAndSaveDefaultAdminUser();
+        UsernamePasswordAuthenticationToken auth = userTestUtil.createAuthenticationToken(adminUser);
 
         ArrayList<String> authorities = new ArrayList<>();
         authorities.add("ROLE_EMPLOYEE");
 
-        userTestUtil.createAndSaveCustomUser(
+        User user1 = userTestUtil.createAndSaveCustomUser(
                 "john1",
                 "doe1",
                 "johndoe1@email.com",
                 "password",
                 authorities
         );
-        userTestUtil.createAndSaveCustomUser(
+        User user2 = userTestUtil.createAndSaveCustomUser(
                 "john2",
                 "doe2",
                 "johndoe2@email.com",
@@ -71,16 +75,18 @@ public class AdminControllerIntegrationTest {
                 authorities
         );
 
-        mockMvc.perform(get("/api/todo")
-                .with(request -> {
-                    SecurityContextHolder.getContext().setAuthentication(auth);
-                    return request;
-                }))
+        mockMvc.perform(get("/api/admin")
+                        .with(request -> {
+                            SecurityContextHolder.getContext().setAuthentication(auth);
+                            return request;
+                        }))
                 .andExpect(status().isOk())
-                .andExpect()
-
-
-
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(3))
+                // Check all emails exist
+                .andExpect(jsonPath("$[?(@.email == '" + adminUser.getEmail() + "')]").exists())
+                .andExpect(jsonPath("$[?(@.email == 'johndoe1@email.com')]").exists())
+                .andExpect(jsonPath("$[?(@.email == 'johndoe2@email.com')]").exists());
     }
 
 
